7bf39018852f45cc2c5ffda298fe9b1caf99a3a1
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-30 18:19:12 +0600

    updating some items

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -10,9 +10,9 @@ const Cart = (props) => {
     const totalPrice = cart.reduce((total, prb) => total + prb.price,0)
     return (
         <div className="cartstyle">
-           <h2>Order Summary</h2>
+           <h2 className="text-danger">Order Summary</h2>
            <h4>Items Ordered:{cart.length} </h4>
-             <p>total price :{totalPrice}</p>
+             <h5 className="text-primary">Total Price :{totalPrice}</h5>
         </div>
     );
 };
